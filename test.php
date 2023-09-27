47c490fbd6cd82ee5d36b9021518abec0ef94de3
Yichi <sss930608@gmail.com>
2023-09-27 21:33:29 +0800

    session_完成

--- test.php
+++ test.php
@@ -28,31 +28,77 @@ if (isset($_POST['submit']))
                 }
             }
         }
-        $_SESSION['arr']=$rand;3
+        $_SESSION['arr']=$rand;
+
 //------------------------------計算次數-----------------------------------
 
-            if (!isset($_SESSION['time']))
-            {
-                $_SESSION['time'] = 0;
-            }
+        if (!isset($_SESSION['time']))
+        {
+            $_SESSION['time'] = 0;
+        }
 
-            if (isset($_POST['submit']))
-            {
-                $_SESSION['time']+=1;
-            }
-//-------------------------------計算相等-----------------------------------------------------------------------
-        $result = abs($_SESSION['arr'][1] - $_SESSION['arr'][0]) ==  abs($_SESSION['arr'][2] - $_SESSION['arr'][1]);
-    }
+        else
+        {
+            $_SESSION['time'] += 1;
+        }
 //------------------------------確認是第幾次--------------------------------------------------------------------
 
-if(!isset($_SESSION['check']))
-{
-    $_SESSION['check']=0;
-}
-else
-{
-    $_SESSION['check']=1;
-}
+        if(!isset($_SESSION['check']))
+        {
+            $_SESSION['check']=0;
+        }
+        else
+        {
+            $_SESSION['check']=1;
+        }
+
+//-------------------------------計算相等-----------------------------------------------------------------------
+        
+        $result = abs($_SESSION['arr'][1] - $_SESSION['arr'][0]) ==  abs($_SESSION['arr'][2] - $_SESSION['arr'][1]);
+
+//-------------------------------解答------------------------------------------------------------------------------
+
+        if(!isset($_SESSION['ans']))
+        {
+            $_SESSION['ans'] = "";
+        }
+
+        $_SESSION['ans'].=$_SESSION['time']."=";
+        for($i=0;$i<count($_SESSION['arr']);$i++)
+        {
+            $_SESSION['ans'] .= $_SESSION['arr'][$i].",";
+        }
+        $_SESSION['ans'].= "</br>";
+
+        if($_SESSION['time'] <= 9)
+        {
+            if($result)
+            {
+                $_SESSION['ans'] .= "總共試了".$_SESSION['time']."次或成功";
+                session_destroy();
+            }
+        }
+        else
+        {
+            $_SESSION['ans'] .= "總共試了".$_SESSION['time']."次或成功";
+            session_destroy();
+        }
+
+    }
+//--------------------------------檢查是否為第一次-----------------------------------------------------------------------------
+    if(isset($_SESSION['check']))
+    {
+		if($_SESSION['check'] == 1)
+        {
+			print_r($_SESSION['ans']);
+		}
+		else
+        {
+			print_r($_SESSION['arr']);//陣列輸出要用print_r
+			echo "</br>";
+		}
+	}
+
 ?>
 </form>
 </body>
